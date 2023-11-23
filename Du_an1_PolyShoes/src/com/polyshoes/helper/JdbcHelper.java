b65f513bb8890887da8b5573d9c5fb0b087bae2a
Vuhieu123 <hieuvutrong281@gmail.com>
2023-11-23 09:04:45 +0700

    hoàn thành export và import

---
+++ Du_an1_PolyShoes/src/com/polyshoes/helper/JdbcHelper.java
@@ -0,0 +1,71 @@
+package com.polyshoes.helper;
+
+import java.awt.Component;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class JdbcHelper {
+
+     private static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        private static final String DBURL = "jdbc:sqlserver://localhost;database=N5_PolyShoes";
+        private static final String USERNAME = "sa";
+        private static final String PASSWORD = "abc@123";
+        
+        static {
+                try {
+                        Class.forName(DRIVER);
+                } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                }
+        }
+
+
+        public static void main(String[] args) throws SQLException {
+                Connection con = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+                DatabaseMetaData dbmt = con.getMetaData();
+                System.out.println(dbmt.getDriverName());
+                System.out.println(dbmt.getDatabaseProductName());
+                System.out.println(dbmt.getDatabaseProductVersion());
+        }
+        
+        public static PreparedStatement preparedStatement(String sql, Object...args) throws SQLException {
+                Connection con = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+                PreparedStatement ps = null;
+                if(sql.trim().startsWith("{")) {
+                        ps = con.prepareCall(sql);
+                } else {
+                        ps = con.prepareStatement(sql);
+                }
+                for (int i = 0; i < args.length; i++) {
+                        ps.setObject(i+1, args[i]);
+                }
+                return ps;
+        }
+        
+        public static void executeUpdate(String sql, Object...args) {
+                try {
+                        PreparedStatement ps = preparedStatement(sql, args);
+                        try {
+                                ps.executeUpdate();
+                        } finally {
+                                ps.getConnection().close();
+                        }
+                } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                }
+        }
+        
+        public static ResultSet executeQuery(String sql, Object...args) {
+                try {
+                        PreparedStatement ps = preparedStatement(sql, args);
+                        return ps.executeQuery();
+                } catch (SQLException e) {
+                        throw new RuntimeException();
+                }
+        }
+}
