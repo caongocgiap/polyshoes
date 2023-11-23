86f8e34990e589550bfe948694239e76e6ad4767
Vuhieu123 <hieuvutrong281@gmail.com>
2023-11-23 10:54:49 +0700

    hoàn thành hết các chức năng

--- Du_an1_PolyShoes/src/com/polyshoes/main/MainJFrame.java
+++ Du_an1_PolyShoes/src/com/polyshoes/main/MainJFrame.java
@@ -11,8 +11,8 @@ import com.polyshoes.swing.MenuItem;
 import com.polyshoes.swing.PopupMenu;
 import com.polyshoes.swing.icon.GoogleMaterialDesignIcons;
 import com.polyshoes.swing.icon.IconFontSwing;
-import com.polyshoes.view.sanpham.QuanLySPJPanel;
-import com.polyshoes.view.sanpham.QuanLySanPhamJPanel;
+import com.polyshoes.view.nhanvien.NhanVienJPanel;
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
@@ -24,101 +24,101 @@ import org.jdesktop.animation.timing.TimingTargetAdapter;
 
 public class MainJFrame extends javax.swing.JFrame {
 
-        private MigLayout layout;
-        private MenuJPanel menu;
-        private HeaderJPanel header;
-        private MainJPanel main;
-        private Animator animator;
+    private MigLayout layout;
+    private MenuJPanel menu;
+    private HeaderJPanel header;
+    private MainJPanel main;
+    private Animator animator;
 
-        public MainJFrame() {
-                initComponents();
-                init();
-        }
+    public MainJFrame() {
+        initComponents();
+        init();
+    }
 
-        private void init() {
-                layout = new MigLayout("fill", "0[]0[100%, fill]0", "0[fill, top]0");
-                bg.setBackground(Color.WHITE);
-                bg.setLayout(layout);
-                menu = new MenuJPanel();
-                header = new HeaderJPanel();
-                main = new MainJPanel();
-                menu.addEvent(new EventMenuSelected() {
-                        @Override
-                        public void menuSelected(int menuIndex, int subMenuIndex) {
-                                System.out.println("Menu Index : " + menuIndex + " SubMenu Index " + subMenuIndex);
-                                if (menuIndex == 2) {
-                                        if (subMenuIndex == 0) {
-                                                main.showForm(new TrangChuJPanel());
-                                        } else if (subMenuIndex == 1) {
-                                                main.showForm(new Form_1());
-                                        }
-                                }
-                                if(menuIndex == 0) {
-                                        main.showForm(new QuanLySanPhamJPanel());
-                                }
-                                if(menuIndex == 6) {
-                                        main.showForm(new QuanLySPJPanel());
-                                }
-                        }
-                });
-                menu.addEventShowPopup(new EventShowPopupMenu() {
-                        @Override
-                        public void showPopup(Component com) {
-                                MenuItem item = (MenuItem) com;
-                                PopupMenu popup = new PopupMenu(MainJFrame.this, item.getIndex(), item.getEventSelected(), item.getMenu().getSubMenu());
-                                int x = MainJFrame.this.getX() + 52;
-                                int y = MainJFrame.this.getY() + com.getY() + 86;
-                                popup.setLocation(x, y);
-                                popup.setVisible(true);
-                        }
-                });
-                menu.initMenuItem();
-                bg.add(menu, "w 230!, spany 2");    // Span Y 2cell
-                bg.add(header, "h 50!, wrap");
-                bg.add(main, "w 100%, h 100%");
-                TimingTarget target = new TimingTargetAdapter() {
-                        @Override
-                        public void timingEvent(float fraction) {
-                                double width;
-                                if (menu.isShowMenu()) {
-                                        width = 60 + (170 * (1f - fraction));
-                                } else {
-                                        width = 60 + (170 * fraction);
-                                }
-                                layout.setComponentConstraints(menu, "w " + width + "!, spany2");
-                                menu.revalidate();
-                        }
+    private void init() {
+        layout = new MigLayout("fill", "0[]0[100%, fill]0", "0[fill, top]0");
+        bg.setBackground(Color.WHITE);
+        bg.setLayout(layout);
+        menu = new MenuJPanel();
+        header = new HeaderJPanel();
+        main = new MainJPanel();
+        menu.addEvent(new EventMenuSelected() {
+            @Override
+            public void menuSelected(int menuIndex, int subMenuIndex) {
+                System.out.println("Menu Index : " + menuIndex + " SubMenu Index " + subMenuIndex);
+                if (menuIndex == 9) {
+                    if (subMenuIndex == 9) {
+                        main.showForm(new NhanVienJPanel());
+                    } else if (subMenuIndex == 0) {
+                        main.showForm(new NhanVienJPanel());
+                    }
+                }
+//                                if(menuIndex == 0) {
+//                                        main.showForm(new QuanLySanPhamJPanel());
+//                                }
+//                                if(menuIndex == 6) {
+//                                        main.showForm(new QuanLySPJPanel());
+//                                }
+            }
+        });
+        menu.addEventShowPopup(new EventShowPopupMenu() {
+            @Override
+            public void showPopup(Component com) {
+                MenuItem item = (MenuItem) com;
+                PopupMenu popup = new PopupMenu(MainJFrame.this, item.getIndex(), item.getEventSelected(), item.getMenu().getSubMenu());
+                int x = MainJFrame.this.getX() + 52;
+                int y = MainJFrame.this.getY() + com.getY() + 86;
+                popup.setLocation(x, y);
+                popup.setVisible(true);
+            }
+        });
+        menu.initMenuItem();
+        bg.add(menu, "w 230!, spany 2");    // Span Y 2cell
+        bg.add(header, "h 50!, wrap");
+        bg.add(main, "w 100%, h 100%");
+        TimingTarget target = new TimingTargetAdapter() {
+            @Override
+            public void timingEvent(float fraction) {
+                double width;
+                if (menu.isShowMenu()) {
+                    width = 60 + (170 * (1f - fraction));
+                } else {
+                    width = 60 + (170 * fraction);
+                }
+                layout.setComponentConstraints(menu, "w " + width + "!, spany2");
+                menu.revalidate();
+            }
 
-                        @Override
-                        public void end() {
-                                menu.setShowMenu(!menu.isShowMenu());
-                                menu.setEnableMenu(true);
-                        }
+            @Override
+            public void end() {
+                menu.setShowMenu(!menu.isShowMenu());
+                menu.setEnableMenu(true);
+            }
 
-                };
-                animator = new Animator(500, target);
-                animator.setResolution(0);
-                animator.setDeceleration(0.5f);
-                animator.setAcceleration(0.5f);
-                header.addMenuEvent(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent ae) {
-                                if (!animator.isRunning()) {
-                                        animator.start();
-                                }
-                                menu.setEnableMenu(false);
-                                if (menu.isShowMenu()) {
-                                        menu.hideallMenu();
-                                }
-                        }
-                });
-                //  Init google icon font
-                IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
-                //  Start with this form
-                main.showForm(new TrangChuJPanel());
-        }
+        };
+        animator = new Animator(500, target);
+        animator.setResolution(0);
+        animator.setDeceleration(0.5f);
+        animator.setAcceleration(0.5f);
+        header.addMenuEvent(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (!animator.isRunning()) {
+                    animator.start();
+                }
+                menu.setEnableMenu(false);
+                if (menu.isShowMenu()) {
+                    menu.hideallMenu();
+                }
+            }
+        });
+        //  Init google icon font
+        IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
+        //  Start with this form
+        main.showForm(new TrangChuJPanel());
+    }
 
-        @SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
@@ -155,39 +155,39 @@ public class MainJFrame extends javax.swing.JFrame {
                 setLocationRelativeTo(null);
         }// </editor-fold>//GEN-END:initComponents
 
-        public static void main(String args[]) {
-                /* Set the Nimbus look and feel */
-                //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-                /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-                 */
-                try {
-                        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                                if ("Nimbus".equals(info.getName())) {
-                                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                                        break;
-                                }
-                        }
-                } catch (ClassNotFoundException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (InstantiationException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (IllegalAccessException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
                 }
-                //</editor-fold>
-                //</editor-fold>
-
-                /* Create and display the form */
-                java.awt.EventQueue.invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                                new MainJFrame().setVisible(true);
-                        }
-                });
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainJFrame().setVisible(true);
+            }
+        });
+    }
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JLayeredPane bg;
