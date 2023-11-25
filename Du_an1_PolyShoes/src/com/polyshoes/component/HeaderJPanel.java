8d63c1786c078dbb2c8dd51c204fe9fbf7b49746
Cao Ngoc Giap <caongocgiap732004@gmail.com>
2023-11-26 00:22:34 +0700

    Thêm khách hàng

--- Du_an1_PolyShoes/src/com/polyshoes/component/HeaderJPanel.java
+++ Du_an1_PolyShoes/src/com/polyshoes/component/HeaderJPanel.java
@@ -1,13 +1,11 @@
 package com.polyshoes.component;
 
-import java.awt.Color;
 import java.awt.event.ActionListener;
 
 public class HeaderJPanel extends javax.swing.JPanel {
 
     public HeaderJPanel() {
         initComponents();
-            setBackground(Color.decode("#FFFFFF"));
     }
 
     public void addMenuEvent(ActionListener event) {
@@ -15,72 +13,68 @@ public class HeaderJPanel extends javax.swing.JPanel {
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
 
-        cmdMenu = new com.polyshoes.swing.Button();
-        pic = new com.polyshoes.swing.ImageAvatar();
-        lbUserName = new javax.swing.JLabel();
-        lbRole = new javax.swing.JLabel();
-        jSeparator1 = new javax.swing.JSeparator();
+                cmdMenu = new com.polyshoes.swing.Button();
+                pic = new com.polyshoes.swing.ImageAvatar();
+                lbUserName = new javax.swing.JLabel();
+                lbRole = new javax.swing.JLabel();
+                jSeparator1 = new javax.swing.JSeparator();
 
-<<<<<<< HEAD
-        setBackground(new java.awt.Color(255, 255, 255));
+                setBackground(new java.awt.Color(255, 255, 255));
 
-        cmdMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/polyshoes/icon/menu.png"))); // NOI18N
-=======
                 cmdMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/polyshoes/icon/menu.png"))); // NOI18N
->>>>>>> 53974e1d02cacb0c223d1a8a3c85d9d54739dbaa
 
-        pic.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/polyshoes/icon/avatar.jpg"))); // NOI18N
+                pic.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/polyshoes/icon/avatar.jpg"))); // NOI18N
 
-        lbUserName.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
-        lbUserName.setForeground(new java.awt.Color(127, 127, 127));
-        lbUserName.setText("Ngọc Giáp");
+                lbUserName.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+                lbUserName.setForeground(new java.awt.Color(127, 127, 127));
+                lbUserName.setText("Ngọc Giáp");
 
-        lbRole.setForeground(new java.awt.Color(127, 127, 127));
-        lbRole.setText("Admin");
+                lbRole.setForeground(new java.awt.Color(127, 127, 127));
+                lbRole.setText("Admin");
 
-        jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
+                jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(cmdMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 459, Short.MAX_VALUE)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 8, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbUserName, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lbRole, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addGap(18, 18, 18)
-                .addComponent(pic, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lbUserName)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(lbRole))
-                    .addComponent(cmdMenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(pic, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jSeparator1))
-                .addContainerGap())
-        );
-    }// </editor-fold>//GEN-END:initComponents
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+                this.setLayout(layout);
+                layout.setHorizontalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(cmdMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 459, Short.MAX_VALUE)
+                                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 8, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(lbUserName, javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addComponent(lbRole, javax.swing.GroupLayout.Alignment.TRAILING))
+                                .addGap(18, 18, 18)
+                                .addComponent(pic, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap())
+                );
+                layout.setVerticalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(lbUserName)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                .addComponent(lbRole))
+                                        .addComponent(cmdMenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(pic, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jSeparator1))
+                                .addContainerGap())
+                );
+        }// </editor-fold>//GEN-END:initComponents
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private com.polyshoes.swing.Button cmdMenu;
-    private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JLabel lbRole;
-    private javax.swing.JLabel lbUserName;
-    private com.polyshoes.swing.ImageAvatar pic;
-    // End of variables declaration//GEN-END:variables
+        // Variables declaration - do not modify//GEN-BEGIN:variables
+        private com.polyshoes.swing.Button cmdMenu;
+        private javax.swing.JSeparator jSeparator1;
+        private javax.swing.JLabel lbRole;
+        private javax.swing.JLabel lbUserName;
+        private com.polyshoes.swing.ImageAvatar pic;
+        // End of variables declaration//GEN-END:variables
 }
--- Du_an1_PolyShoes/src/com/polyshoes/swing/table/ModelAction.java
+++ Du_an1_PolyShoes/src/com/polyshoes/swing/table/ModelAction.java
@@ -1,34 +1,38 @@
 package com.polyshoes.swing.table;
 
-import com.polyshoes.model.sanpham.SanPham;
+import com.polyshoes.model.khachhang.KhachHang;
 
 
 public class ModelAction {
 
-        public SanPham getSanPham() {
-                return sanPham;
-        }
+    public ModelAction(KhachHang aThis, EventAction event) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+//
+//        public KhachHang getKhachHang() {
+//                return KhachHang;
+//        }
+//
+//        public void setKhachHang(KhachHang kh) {
+//                this.KhachHang = KhachHang;
+//        }
+//
+//        public EventAction getEvent() {
+//                return event;
+//        }
+//
+//        public void setEvent(EventAction event) {
+//                this.event = event;
+//        }
+//
+//        public ModelAction(KhachHang KhachHang, EventAction) {
+//                this.KhachHang = KhachHang ;
+//                this.event = event;
+//        }
+//
+//        public ModelAction() {
+//        }
 
-        public void setSanPam(SanPham sanPham) {
-                this.sanPham = sanPham;
-        }
-
-        public EventAction getEvent() {
-                return event;
-        }
-
-        public void setEvent(EventAction event) {
-                this.event = event;
-        }
-
-        public ModelAction(SanPham sanPham, EventAction event) {
-                this.sanPham = sanPham;
-                this.event = event;
-        }
-
-        public ModelAction() {
-        }
-
-        private SanPham sanPham;
+        private KhachHang KhachHang;
         private EventAction event;
 }
