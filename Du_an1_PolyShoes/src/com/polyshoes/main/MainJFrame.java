0e87d3f45bf74a6da94c413f5468b4f83fd4aa1f
Cao Ngoc Giap <caongocgiap732004@gmail.com>
2023-11-26 00:08:51 +0700

    Thêm khách hàng

--- Du_an1_PolyShoes/src/com/polyshoes/main/MainJFrame.java
+++ Du_an1_PolyShoes/src/com/polyshoes/main/MainJFrame.java
@@ -4,20 +4,15 @@ import com.polyshoes.component.HeaderJPanel;
 import com.polyshoes.component.MenuJPanel;
 import com.polyshoes.event.EventMenuSelected;
 import com.polyshoes.event.EventShowPopupMenu;
+import com.polyshoes.view.trangchu.QLSPJPanel;
 import com.polyshoes.view.trangchu.TrangChuJPanel;
 import com.polyshoes.view.trangchu.MainJPanel;
 import com.polyshoes.swing.MenuItem;
 import com.polyshoes.swing.PopupMenu;
 import com.polyshoes.swing.icon.GoogleMaterialDesignIcons;
 import com.polyshoes.swing.icon.IconFontSwing;
-import com.polyshoes.view.glasspanepopup.GlassPanePopup;
-import com.polyshoes.view.khuyenmai.QuanLyKhuyenMaiPanel;
-import com.polyshoes.view.khuyenmai.QuanLyKhuyenMaiPanellll;
-import com.polyshoes.view.HoaDon.HoaDonPanel10;
-import com.polyshoes.view.banhang.BanHangJPanel;
-import com.polyshoes.view.nhanvien.NhanVienJPanel;
-import com.polyshoes.view.sanpham.QLSPJPanel;
-import java.awt.Color;
+
+import com.polyshoes.view.khachhang.KhacHangJPanel;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -28,18 +23,16 @@ import org.jdesktop.animation.timing.TimingTargetAdapter;
 
 public class MainJFrame extends javax.swing.JFrame {
 
-        private MigLayout layout;
-        private MenuJPanel menu;
-        private HeaderJPanel header;
-        private MainJPanel main;
-        private Animator animator;
+    private MigLayout layout;
+    private MenuJPanel menu;
+    private HeaderJPanel header;
+    private MainJPanel main;
+    private Animator animator;
 
     public MainJFrame() {
         initComponents();
         init();
-        GlassPanePopup.install(this);
     }
-
 
     private void init() {
         layout = new MigLayout("fill", "0[]0[100%, fill]0", "0[fill, top]0");
@@ -58,15 +51,12 @@ public class MainJFrame extends javax.swing.JFrame {
                         main.showForm(new QLSPJPanel());
                     }
                 }
-                if (menuIndex == 7) {
+                if (menuIndex == 4) {
                     if (subMenuIndex == 0) {
-                        main.showForm(new TrangChuJPanel());
-                    }else if (subMenuIndex == 1) {
-                        main.showForm(new QuanLyKhuyenMaiPanellll());
+                        main.showForm(new KhacHangJPanel());
+                    } else if (subMenuIndex == 1) {
+                      main.showForm(new KhacHangJPanel());
                     }
-                }
-                if (menuIndex == 1) {
-                    main.showForm(new QuanLyKhuyenMaiPanel());
                 }
             }
         });
@@ -98,107 +88,109 @@ public class MainJFrame extends javax.swing.JFrame {
                 menu.revalidate();
             }
 
-                        @Override
-                        public void end() {
-                                menu.setShowMenu(!menu.isShowMenu());
-                                menu.setEnableMenu(true);
-                        }
+            @Override
+            public void end() {
+                menu.setShowMenu(!menu.isShowMenu());
+                menu.setEnableMenu(true);
+            }
 
-                };
-                animator = new Animator(500, target);
-                animator.setResolution(0);
-                animator.setDeceleration(0.5f);
-                animator.setAcceleration(0.5f);
-                header.addMenuEvent(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent ae) {
-                                if (!animator.isRunning()) {
-                                        animator.start();
-                                }
-                                menu.setEnableMenu(false);
-                                if (menu.isShowMenu()) {
-                                        menu.hideallMenu();
-                                }
-                        }
-                });
-                //  Init google icon font
-                IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
-                //  Start with this form
-                main.showForm(new TrangChuJPanel());
-        }
-
-        @SuppressWarnings("unchecked")
-        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-        private void initComponents() {
-
-                bg = new javax.swing.JLayeredPane();
-
-                setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-                bg.setBackground(new java.awt.Color(245, 245, 245));
-                bg.setOpaque(true);
-
-                javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
-                bg.setLayout(bgLayout);
-                bgLayout.setHorizontalGroup(
-                        bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 1366, Short.MAX_VALUE)
-                );
-                bgLayout.setVerticalGroup(
-                        bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 783, Short.MAX_VALUE)
-                );
-
-                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-                getContentPane().setLayout(layout);
-                layout.setHorizontalGroup(
-                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(bg)
-                );
-                layout.setVerticalGroup(
-                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(bg)
-                );
-
-                pack();
-                setLocationRelativeTo(null);
-        }// </editor-fold>//GEN-END:initComponents
-
-        public static void main(String args[]) {
-                /* Set the Nimbus look and feel */
-                //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-                /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-                 */
-                try {
-                        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                                if ("Nimbus".equals(info.getName())) {
-                                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                                        break;
-                                }
-                        }
-                } catch (ClassNotFoundException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (InstantiationException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (IllegalAccessException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-                        java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        };
+        animator = new Animator(500, target);
+        animator.setResolution(0);
+        animator.setDeceleration(0.5f);
+        animator.setAcceleration(0.5f);
+        header.addMenuEvent(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (!animator.isRunning()) {
+                    animator.start();
                 }
-                //</editor-fold>
-                //</editor-fold>
+                menu.setEnableMenu(false);
+                if (menu.isShowMenu()) {
+                    menu.hideallMenu();
+                }
+            }
+        });
+        //  Init google icon font
+        IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
+        //  Start with this form
+        main.showForm(new TrangChuJPanel());
 
-                /* Create and display the form */
-                java.awt.EventQueue.invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                                new MainJFrame().setVisible(true);
-                        }
-                });
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg = new javax.swing.JLayeredPane();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        bg.setBackground(new java.awt.Color(245, 245, 245));
+        bg.setEnabled(false);
+        bg.setOpaque(true);
+
+        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
+        bg.setLayout(bgLayout);
+        bgLayout.setHorizontalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1366, Short.MAX_VALUE)
+        );
+        bgLayout.setVerticalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 783, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
 
-        // Variables declaration - do not modify//GEN-BEGIN:variables
-        private javax.swing.JLayeredPane bg;
-        // End of variables declaration//GEN-END:variables
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainJFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLayeredPane bg;
+    // End of variables declaration//GEN-END:variables
 }
---
+++ Du_an1_PolyShoes/src/com/polyshoes/swing/table/TableActionEvent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.polyshoes.swing.table;
+
+import com.polyshoes.model.khachhang.KhachHang;
+
+/**
+ *
+ * @author ADMIN
+ */
+public interface TableActionEvent {
+    public void onEdit(int row);
+
+}
